d7258b433a5811ca748762718eb725a4bb0a5c48
Essayons Change <psmith@essayonschange.com>
2026-07-21 07:41:11 -0500

    Add files via upload

---
+++ client/src/lib/booking.ts
@@ -0,0 +1,6 @@
+/**
+ * Scheduling link for consultations. Centralized so the URL can be updated
+ * in one place if the Calendly event or account changes.
+ */
+export const CONSULTATION_URL =
+  "https://calendly.com/psmith-essayonschange/introductory-meeting-with-essayons-change";
