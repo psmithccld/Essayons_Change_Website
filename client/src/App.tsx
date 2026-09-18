a6ef0e8e53a6a0bf73058c20738897573596cc99
Essayons Change <psmith@essayonschange.com>
2026-09-18 07:17:55 -0500

    Redirect '/pricing' to '/offerings'
    Redirect '/pricing' route to '/offerings' instead of rendering Pricing component.

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import { Switch, Route } from "wouter";
+import { Switch, Route, Redirect } from "wouter";
 import { queryClient } from "./lib/queryClient";
 import { QueryClientProvider } from "@tanstack/react-query";
 import { Toaster } from "@/components/ui/toaster";
@@ -7,7 +7,6 @@ import Header from "@/components/Header";
 import Footer from "@/components/Footer";
 import Home from "@/pages/home";
 import About from "@/pages/about";
-import Pricing from "@/pages/pricing";
 import Blog from "@/pages/blog";
 import Tutorials from "@/pages/tutorials";
 import Contact from "@/pages/contact";
@@ -54,7 +53,9 @@ function Router() {
                 <Route path="/model" component={Model} />
                 <Route path="/offerings" component={Offerings} />
                 <Route path="/partners" component={Partners} />
-                <Route path="/pricing" component={Pricing} />
+                <Route path="/pricing">
+                  <Redirect to="/offerings" />
+                </Route>
                 <Route path="/privacy" component={Privacy} />
                 <Route path="/terms" component={Terms} />
                 <Route path="/investor" component={Investor} />
